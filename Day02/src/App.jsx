20425cfc6c43d437d1c9376fe9c391714f2f93db
MdFirozAlam-maker <ifirozalam0786@gmail.com>
2026-05-04 16:06:23 +0530

    Demonstrated JSX vs React.createElement by rendering both components

--- Day02/src/App.jsx
+++ Day02/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Header from './components/Header'
 import Card from './components/Card'
+import { Hello , HelloWithoutJSX} from './Hello'
+
  
 
 const App = () => {
@@ -8,6 +10,8 @@ const App = () => {
  
   return (
     <div>
+      <Hello />
+      <HelloWithoutJSX />
       <Header />
       <Card title="React Learning" />
       <Card title="Components" />
